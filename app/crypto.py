202fbd709605f55c7471229309b1873cdd231432
taismoreira2023 <projetos174@gmail.com>
2024-11-30 15:17:25 -0300

    Revert "Merge branch 'fernando' of github.com:Joao-ava/analise_combinatoria"
    This reverts commit cbb93b111a7aea0de0df4fe69f67461f81e469f0, reversing
    changes made to dd8d959ac56799d7f753ba5102aee9c8422ee788.

--- app/crypto.py
+++ app/crypto.py
@@ -1,27 +1,10 @@
-from app.core import EncryptWord, letters_distance
+from app.core import EncryptWord
 
 
 class Crypto:
     @staticmethod
     def find_key(original: str, encrypt: str) -> int:
-        encrypt_text = EncryptWord(encrypt)
-        original_text = EncryptWord(original)
-        for i in range(1, 6):
-            if (original_text + i) == encrypt_text:
-                return i
-
-        # diff = letters_distance(EncryptWord(original), EncryptWord(encrypt))
-        # pattern = []
-        # #find pattern in list
-        # for i in range(5,1,-1):
-        #     print(f"i: {i}")
-        #     # get first i items for diff
-        #     find_pattern = diff[:i]
-        #     print(f"find pattern {find_pattern}, diff: {diff[i:i*2]}")
-        #     if find_pattern == diff[i:i*2]:
-        #         pattern = find_pattern
-        #         break
-        # return pattern
+        return 0
 
     @staticmethod
     def encrypt(key: int, text: str) -> str:
